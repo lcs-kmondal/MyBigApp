4b09d56c43e8108a7f98b0327f3d9e4a303f2ab3
Kalyan Mondal <kmondal@lcs.on.ca>
2026-03-02 11:53:33 -0500

    made a custom subview for each article and implemented 3 articles

--- MyBigApp/ContentView.swift
+++ MyBigApp/ContentView.swift
@@ -9,15 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        VStack{
+            HStack{
+                article(articleTitle: "Maritime farmers holding breath as record-dry spring wrings region", time: "1h ago")
+                article(articleTitle: "E. Jean Carroll seeks $10 million in damages from Trump ", time: "1h ago")
+            }
+            
+            article(articleTitle: "New Zealand's ratio of sheeps to humans at lowest point in 170 years", time: "1h ago")
         }
+    
         .padding()
     }
 }
+
+
 
 #Preview {
     ContentView()
---
+++ MyBigApp/article.swift
@@ -0,0 +1,34 @@
+//
+//  article.swift
+//  MyBigApp
+//
+//  Created by Xavier Mondal on 2026-03-02.
+//
+
+
+import SwiftUI
+
+struct article: View {
+    let articleTitle: String
+    let time: String
+    var body: some View {
+        VStack(alignment: .leading){
+            Rectangle()
+                .frame(width: 150, height:150)
+            Rectangle()
+                .frame(width: 150, height:50)
+            Text(articleTitle)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.leading)
+                .frame(width:150)
+                
+            Spacer()
+                .frame(height:10)
+            HStack{
+                Text(time)
+                    .multilineTextAlignment(.leading)
+                Spacer()
+            }
+        }
+    }
+}
