f6c5e794b4d2af23ecd2387b1a1a0ff008cd8dae
Kalyan Mondal <kmondal@lcs.on.ca>
2026-03-02 11:05:22 -0500

    Initial Commit

---
+++ MyBigApp/MyBigAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBigAppApp.swift
+//  MyBigApp
+//
+//  Created by Xavier Mondal on 2026-03-02.
+//
+
+import SwiftUI
+
+@main
+struct MyBigAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
